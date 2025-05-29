ff4967b8300887e29442897f0a54e93025a78dfc
Stache <stache@stache.cat>
2025-05-29 17:07:58 +0200

    Working base

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,50 @@
-from flask import render_template
-
+from flask import render_template, request, redirect, url_for
 
 from app.main import bp
 
 
-@bp.route("/")
-def index():
-    return render_template("index.html")
+@bp.route("/test", methods=("GET", "POST"))
+def test():
+    # Parameters
+    current_page = "test.html"
+    next_fun = "main.test"
+    chara_pic = "draft_neutral"
+    speech_text = "En train de faire des tests en fait"
+
+    # Current page
+    if request.method == "GET":
+        return render_template(
+            current_page,
+            chara_pic=chara_pic,
+            speech_text=speech_text,
+        )
+    # Next page
+
+    if request.method == "POST":
+        return redirect(url_for(next_fun))
+
+    # Error
+    return redirect(url_for("main.entry"))
+
+@bp.route("/", methods=("GET", "POST"))
+def entry():
+    # Parameters
+    current_page = "entry.html"
+    next_fun = "main.test"
+    chara_pic = "draft_neutral"
+    speech_text = ""
+
+    # Current page
+    if request.method == "GET":
+        return render_template(
+            current_page,
+            chara_pic=chara_pic,
+            speech_text=speech_text,
+        )
+    # Next page
+
+    if request.method == "POST":
+        return redirect(url_for(next_fun))
+
+    # Error
+    return redirect(url_for("main.entry"))
---
+++ app/main/utilities.py
@@ -0,0 +1,25 @@
+import bcrypt
+import hashlib
+from zxcvbn import zxcvbn
+
+def hash_password(password: str) -> (bytes, bytes):
+    password_encoded = password.encode("utf-8")
+    salt = bcrypt.gensalt(10)
+    bcrypt_hash = bcrypt.hashpw(password_encoded, salt)
+    md5_hash = hashlib.md5(password_encoded).digest()
+
+    return md5_hash, bcrypt_hash
+
+def check_hash(password: str, md5_hash: bytes, bcrypt_hash: bytes) -> (bool, bool):
+    password_encoded = password.encode("utf-8")
+    return md5_hash == hashlib.md5(password_encoded).digest(), bcrypt.checkpw(password_encoded, bcrypt_hash)
+
+md5_hash, bcrypt_hash = hash_password("TEST")
+
+print(md5_hash, bcrypt_hash)
+
+# Check False
+print(check_hash("test", md5_hash, bcrypt_hash))
+
+# Check True
+print(check_hash("TEST", md5_hash, bcrypt_hash))
